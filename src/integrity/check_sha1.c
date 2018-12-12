4dbd0fd8efda7fcfb8009a6fca3347cdeb59a658
Thomas Lupin <thomas.lupin@epita.fr>
2018-12-12 10:48:19 +0100

    [INTEGRITY] Fix integrity when \0 in hash

--- src/integrity/check_sha1.c
+++ src/integrity/check_sha1.c
@@ -8,6 +8,31 @@
 
 #include "parser.h"
 
+static void unfix_string(struct metainfo *meta, unsigned char *sha1, size_t nb)
+{
+    char *str = meta->pieces + nb * 20;
+    size_t i = 0;
+    for (size_t j = 0; j < 20; i++, j++)
+    {
+        if (str[i] == 'U' && str[i + 1] == '+' && str[i + 2] == '0'
+                && str[i + 3] == '0' && str[i + 4] && str[i + 5])
+        {
+            char tmp[3] =
+            {
+                str[i + 4], str[i + 5], '\0'
+            };
+            unsigned int hex;
+            sscanf(tmp, "%02X", &hex);
+            sha1[j] = hex;
+            i += 5;
+        }
+        else
+        {
+            sha1[j] = str[i];
+        }
+    }
+}
+
 static size_t get_total_size(struct metainfo *meta)
 {
     size_t acu = 0;
@@ -21,10 +46,8 @@ static size_t get_total_size(struct metainfo *meta)
 static int check_piece(struct metainfo *meta, size_t nb)
 {
     size_t start = nb * meta->piece_size;
-    if (strlen(meta->pieces) < (nb + 1) * 20)
-        return 0;
     unsigned char sha1[20];
-    memcpy(sha1, meta->pieces + nb * 20, 20); // Get sha1 of piece
+    unfix_string(meta, sha1, nb);
     size_t ptr = 0;
     size_t i = 0;
     for (size_t cur = 0; meta->files[i]; i++) // Get position of piece in files
@@ -38,19 +61,25 @@ static int check_piece(struct metainfo *meta, size_t nb)
         if (start == cur)
             break;
     }
-    unsigned char *piece = calloc(meta->piece_size + 1, sizeof(unsigned char));
-    for (size_t k = 0; k < meta->piece_size; k++) // Get piece content
+    unsigned char *piece = malloc(meta->piece_size * sizeof(unsigned char));
+    size_t k = 0;
+    while (k < meta->piece_size && meta->files[i]) // Get piece content
     {
         for (; k < meta->piece_size && meta->files[i]; i++)
         {
             int fd = open(meta->files[i], O_RDONLY);
+            if (ptr)
+            {
+                lseek(fd, ptr, SEEK_SET);
+                ptr = 0;
+            }
             for (unsigned char c; k < meta->piece_size && read(fd, &c, 1); k++)
                 piece[k] = c;
             close(fd);
         }
     }
-    unsigned char hash[SHA_DIGEST_LENGTH];
-    SHA1(piece, meta->piece_size, hash);
+    unsigned char hash[20];
+    SHA1(piece, k, hash);
     free(piece);
     return memcmp(hash, sha1, 20) == 0;
 }
