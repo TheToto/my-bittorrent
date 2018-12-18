00ed91b9b561e2d5d583ef2ca2296dfb4a9d9070
Thomas Lupin <thomas.lupin@epita.fr>
2018-12-18 12:15:16 +0100

    [EPOLL] Wait message to be complete

--- src/peers/epoll.c
+++ src/peers/epoll.c
@@ -74,6 +74,21 @@ static void handle_type_req(struct metainfo *meta, struct peer *peer,
     switch_events(meta, peer, str, ntohl(*len));
 }
 
+static size_t check_size(char *buf, size_t size, struct peer *peer)
+{
+    if (size == 0)
+        return 0;
+    if (peer->handshaked == 0)
+        return buf[0] + 49;
+    void *tmp = buf;
+    uint32_t *len = tmp;
+    uint32_t real_len = ntohl(*len);
+    printf("%zd bytes read, expedted %u.\n\n", size, real_len + 4);
+    if (real_len + 4 <= size)
+        return real_len + 4;
+    return 0;
+}
+
 void wait_event_epoll(struct metainfo *meta)
 {
     int event_count;
@@ -88,11 +103,25 @@ void wait_event_epoll(struct metainfo *meta)
         {
             struct peer *peer = events[i].data.ptr;
             printf("Reading file descriptor '%d' -- ", peer->sockfd);
-            // CHANGER READ POUR RECV
-            size_t bytes_read = read(peer->sockfd, read_buffer, READ_SIZE);
-            printf("%zd bytes read.\n\n", bytes_read);
+
+            // only peek here to check if download is complete
+            size_t bytes_read = recv(peer->sockfd, read_buffer,
+                    READ_SIZE, MSG_PEEK);
             for (size_t i = bytes_read; i < READ_SIZE + 1; i++)
                 read_buffer[i] = '\0';
+
+            size_t to_read = check_size(read_buffer, bytes_read, peer);
+            if (to_read == 0)
+            {
+                printf("Uncomplete msg...\n");
+                continue;
+            }
+            // Really take data here
+            bytes_read = read(peer->sockfd, read_buffer, to_read);
+            for (size_t i = bytes_read; i < READ_SIZE + 1; i++)
+                read_buffer[i] = '\0';
+            if (bytes_read != to_read)
+                errx(1, "NOOOOOO");
             handle_type_req(meta, peer, read_buffer, bytes_read);
         }
     }
