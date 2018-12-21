273d4fde4c8a9c296a8dbda66752c5f8b293b277
Thomas Lupin <thomas.lupin@epita.fr>
2018-12-21 16:42:36 +0100

    [TEST] Fix integrity wrong test

--- test/script/integrity_wrong.sh
+++ test/script/integrity_wrong.sh
@@ -2,8 +2,8 @@
 es=$?
 res=1
 if test $es -eq 1;then
-    if test "$(cat str)" = ""; then
-        if ! test "$(cat empty)" = ""; then
+    if ! test "$(cat str)" = ""; then
+        if test "$(cat empty)" = ""; then
             res=0
         else
             echo Output: $(cat str)
