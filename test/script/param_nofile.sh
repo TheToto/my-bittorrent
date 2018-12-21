d0bc75a8e060c724b8fbedb28d0431e39c93dcfd
Thomas Lupin <thomas.lupin@epita.fr>
2018-12-21 14:26:42 +0100

    [TEST] Fix wrong test in param_no_file

--- test/script/param_nofile.sh
+++ test/script/param_nofile.sh
@@ -3,11 +3,11 @@ es=$?
 res=1
 if test $es -eq 1;then
     if test "$(cat empty)" = ""; then
-        if test "$(cat str)" = "my-bittorrent: Usage: ./my-bittorrent [options] [files]"; then
+        if test "$(cat str)" = "$1: invalid option -- 'n'"; then
             res=0
         else
             echo Output: $(cat str)
-            echo "Ref:    my-bittorrent: Usage: ./my-bittorrent [options] [files]"
+            echo "Ref:    $1: invalid option -- 'n'"
         fi
     else
         echo Stdout should be empty: $(cat empty)
