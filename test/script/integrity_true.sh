416b7a6dfbcc81b4d1f1c16ee2f6c075a5156db4
louis.holleville <louis.holleville@epita.fr>
2018-12-21 15:40:05 +0100

    [TESTSUITE] Added real cases to integrity

---
+++ test/script/integrity_true.sh
@@ -0,0 +1,23 @@
+./$1 "-m" "$2/test/torrents/README" &> /dev/null
+./$1 "-c" "$2/test/torrents/README.torrent" 1> empty 2>str
+es=$?
+res=1
+if test $es -eq 0;then
+    if test "$(cat str)" = ""; then
+        if test "$(cat empty)" = ""; then
+            res=0
+        else
+            echo Output: $(cat str)
+            echo "stdout should be empty"
+        fi
+    else
+        echo Stderr should be empty: $(cat str)
+    fi
+else
+    echo Exit status: $es
+    echo Ref:         0
+fi
+rm empty 2>/dev/null
+rm str 2>/dev/null
+rm $2/test/torrents/README.torrent 2> /dev/null
+exit $res
